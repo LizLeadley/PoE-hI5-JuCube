c7ab518e29640636476ecfc918a9250fa6f37bba
Miranda Lao <mlao@olin.edu>
2017-10-21 16:22:52 -0400

    is actually a file close now

--- OpenFile.py
+++ OpenFile.py
@@ -1,5 +1,7 @@
 #File - Opening a File with Python
 
 import os
-path = 'C:\Users\mlao\Downloads\APril18.mp3'
-os.startfile(path)
+os.system('TASKKILL /F /IM wmplayer.exe')
+
+#path = 'C:\Users\mlao\Downloads\APril18.mp3'
+#os.startfile(path)
