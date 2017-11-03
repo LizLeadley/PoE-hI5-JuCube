c16a61ff3486c91126d7c5e89f9ef4c4e4fb3906
anikapayano <anika.payano@students.olin.edu>
2017-11-03 10:03:22 -0400

    Added music  Player -- works for python3

--- Testcode.py
+++ Testcode.py
@@ -1,19 +1,23 @@
 import serial
 import os
 
-path1 = 'C:\Users\mlao\Desktop\P03\JukeboxHero.mp3'
-path2 = 'C:\Users\mlao\Desktop\P03\ShapeOfYou.mp3'
-arduino = serial.Serial('COM5', 9600, timeout=.1)
+path1 = 'C:/Users/apayano/Music/Music/Breaking_Free_Remix.mp3'
+path2 = 'C:/Users/apayano/Music/Music/Chance The Rapper- Acid Rap/04. Juice.mp3'
+arduino = serial.Serial('COM12', 9600, timeout=.1)
 while True:
 	data = arduino.readline()[:-2]
-	if data == '11':
-		print data
+	if data == b'11':
+		print('yes')
 		os.system('TASKKILL /F /IM wmplayer.exe')
 		os.startfile(path1)
-	if data == '21':
+		print('playing')
+	if data == b'21':
 		os.system('TASKKILL /F /IM wmplayer.exe')
 		os.startfile(path2)
-	if data == '12':
+		print('playing')
+	if data == b'12':
 		os.system('TASKKILL /F /IM wmplayer.exe')
-	if data == '22':
+		print('stop')
+	if data == b'22':
 		os.system('TASKKILL /F /IM wmplayer.exe')
+		print('stop')
