00ac3c65894f5c37ec2be02e4c5ec22e406b94fe
snielsen221b <sophianielsen221b@gmail.com>
2017-11-07 10:48:03 -0500

    Record tilt code done and working!!!

--- Record_Needle_Tilt/Record_Needle_Tilt.ino
+++ Record_Needle_Tilt/Record_Needle_Tilt.ino
@@ -11,19 +11,28 @@ Servo needle_tilt_servo; // creates servo object
 int pos = 0; // position, for servo sweep iterator
 float servo_step = 1; // step size for servo sweep
 unsigned int servo_delay = 20;
+unsigned int prev_millis = 0;
+unsigned int cur_millis = millis();
 
 void setup() {
   needle_tilt_servo.attach(5); // attaches servo to pin 5
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  new_song_tilt(needle_tilt_servo, servo_delay);
-  delay(1000);
+  cur_millis = millis();
+  if (cur_millis - prev_millis > 1000) {
+    prev_millis += 1000;
+    new_song_tilt(needle_tilt_servo, servo_delay);
+  }
+
+//  new_song_tilt(needle_tilt_servo, servo_delay);
+//  delay(500);
+  
 }
 
 int new_song_tilt(Servo needle_tilt_servo, unsigned int servo_delay) {
   tilt_up(needle_tilt_servo, servo_delay);
+  tilt_down(needle_tilt_servo, servo_delay);
 }
 
 int tilt_up(Servo needle_tilt_servo, unsigned int servo_delay) {
@@ -32,16 +41,10 @@ int tilt_up(Servo needle_tilt_servo, unsigned int servo_delay) {
   boolean running_correctly = true;
   boolean needle_move_up_done = false;
   int pos = 0;
-
-  needle_tilt_servo.write(0);
   while(!needle_move_up_done){
-    unsigned int cur_millis = millis();
-    // check if needle has moved up
-    if (servo_step_move_done == true){
-      servo_step_move_done = false;
-    }
+    unsigned int cur_millis = millis(); 
     // if it has, check that the delay has passed
-    else if (cur_millis - prev_millis >= servo_delay) {
+    if (cur_millis - prev_millis >= servo_delay) {
       // update prev_millis
       prev_millis += servo_delay;
       if (++pos > 90) {
@@ -49,6 +52,7 @@ int tilt_up(Servo needle_tilt_servo, unsigned int servo_delay) {
       }
       else {
         needle_tilt_servo.write(pos);
+        servo_step_move_done = true;
         pos ++;
       }
     }
@@ -59,26 +63,21 @@ int tilt_down(Servo needle_tilt_servo, unsigned int servo_delay) {
   unsigned int prev_millis = 0;
   boolean servo_step_move_done = false;
   boolean running_correctly = true;
-  boolean needle_move_up_done = false;
-  int pos = 0;
-
-  needle_tilt_servo.write(0);
-  while(!needle_move_up_done){
+  boolean needle_move_down_done = false;
+  int pos = 90;
+  while(!needle_move_down_done){
     unsigned int cur_millis = millis();
-    // check if needle has moved up
-    if (servo_step_move_done == true){
-      servo_step_move_done = false;
-    }
     // if it has, check that the delay has passed
-    else if (cur_millis - prev_millis >= servo_delay) {
+    if (cur_millis - prev_millis >= servo_delay) {
       // update prev_millis
       prev_millis += servo_delay;
-      if (++pos > 90) {
-        needle_move_up_done = true;
+      if (--pos < 0) {
+        needle_move_down_done = true;
       }
       else {
         needle_tilt_servo.write(pos);
-        pos ++;
+        servo_step_move_done = true;
+        pos --;
       }
     }
 }
