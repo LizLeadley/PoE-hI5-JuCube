916d4450af3ec64ea86c470275b562aa42b4417e
Miranda Lao <mlao@olin.edu>
2017-11-07 18:46:57 -0500

    tried integration and failed lmao but its an attempt

--- LCD/LCD_and_Buttons/LCD_and_Buttons.ino
+++ LCD/LCD_and_Buttons/LCD_and_Buttons.ino
@@ -46,21 +46,20 @@ double blue_b_g = 73/224.0;  //blue ratio for blue to green
 
 // setup routine runs once when you press reset (only runs once)
 void setup() {
+    pinMode(signalRead, INPUT); //set the analog pin as an input
+    // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+    #if defined (__AVR_ATtiny85__)
+      if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+    #endif
+    // End of trinket special code
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
+    strip.setBrightness(240); //set max brightness
+    
     Serial.begin(9600);
     pinMode(button1,INPUT);
     pinMode(button2,INPUT);
     pinMode(button3,INPUT);  
-    
-    //LED STRIP SETUP
-      pinMode(signalRead, INPUT); //set the analog pin as an input
-      // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-      #if defined (__AVR_ATtiny85__)
-        if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-      #endif
-      // End of trinket special code
-      strip.begin();
-      strip.show(); // Initialize all pixels to 'off'
-      strip.setBrightness(240);
     
     lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
   // ------- Quick 3 blinks of backlight  -------------
