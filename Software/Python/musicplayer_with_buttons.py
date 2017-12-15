88fb03ac91979e7109165a2d107f715515a72c50
anikapayano <anika.payano@students.olin.edu>
2017-12-14 20:43:52 -0500

    final python code with comments

--- Software/Python/musicplayer_with_buttons.py
+++ Software/Python/musicplayer_with_buttons.py
@@ -1,120 +1,102 @@
 import pygame
-import multiprocessing
 import os
-import sys
-import random
-import threading
 import time
 import serial
 
 class Music_Player():
 
     def __init__(self):
-        #pygame.mixer.pre_init(48000, -16, 2, 2048)
-        pygame.mixer.init()
-        pygame.init()
-        pygame.font.init()
-        self.display = pygame.display.set_caption('Music Player')
-        self.blue = (0, 100, 205)
-        self.size = [640,480]
-        self.screen = pygame.display.set_mode(self.size)
-        self.clock = pygame.time.Clock()
-        self.myfont = pygame.font.SysFont("freesandbold", 50)
-        self.label = self.myfont.render("SoundCrystal", True, (0,255,0))
+        pygame.mixer.init() #initialize pygame mixer
+        pygame.init() # initialize pygame
+        pygame.font.init() #initialize pygame font
+        self.display = pygame.display.set_caption('Music Player') #set display
+        self.blue = (0, 100, 205) #set color
+        self.size = [640,480] #set size of display
+        self.screen = pygame.display.set_mode(self.size) #open screen
+        self.clock = pygame.time.Clock() #start timer
+        self.myfont = pygame.font.SysFont("freesandbold", 50) #set font
+        self.label = self.myfont.render("SoundCrystal", True, (0,255,0)) #write on screen
 
     def filesfolder(self):
         files = []
-        path = 'C:/Users/apayano/Documents/GitHub/PoE-hI5-JuCube/Music/'
+        path = 'C:/Users/apayano/Documents/GitHub/PoE-hI5-JuCube/Music/' #change path to wherever music files can be found
         directory = os.listdir(path)
         for filename in directory:
-            if filename.endswith(".mp3"):
+            if filename.endswith(".mp3"): #if wav files, change to ".wav"
                 files.append(filename)
         files.sort()
         return files
 
-    def picksong(self):
-        files = self.filesfolder()
-        song = random.choice(files)
-        return song
-
-
-musicplayer = Music_Player()
-songs = musicplayer.filesfolder()
-sound = musicplayer.picksong()
-arduino = serial.Serial('COM11', 9600, timeout=.1)
+arduino = serial.Serial('COM11', 9600, timeout=.1) #initialize Arduino serial port
 
 def events(player, songs):
     i = 0
     loaded = False
-    path = 'C:/Users/apayano/Documents/GitHub/PoE-hI5-JuCube/Music/'
+    song = songs[i]
+    songlen = len(songs[i])
+    songname = song[3:songlen-4]
+
 
     while True:
-        data = arduino.readline()[:-2]
-        END_MUSIC_EVENT = pygame.USEREVENT + 0
-        pygame.mixer.music.set_endevent(END_MUSIC_EVENT)
-        if data == b'1':
+        data = arduino.readline()[:-2] #start reading from Arduino serial
+        #conditions if buttons are pressed
+        if data == b'1': #serial outputs "1" meaning back button is pressed
             i -= 1
             if i < 0:
                 i = 15
             print(songs[i])
-            song = songs[i]
-            songlen = len(songs[i])
-            songname = song[3:songlen-4]
             player.label = player.myfont.render(songname, True, (0,255,0))
             print("Back")
-        if data == b'4':
+        if data == b'4': #serial outputs "4" meaning forward button is pressed
             i += 1
             print(i)
             if i > 15:
                 i = 0
             print(songs[i])
-            song = songs[i]
-            songlen = len(songs[i])
-            songname = song[3:songlen-4]
             player.label = player.myfont.render(songname, True, (0,255,0))
             print("Forward")
-        if data == b'2':
+        if data == b'2': #serial outputs "2" meaning load button is pressed
             print("Loading " + songs[i])
             player.label = player.myfont.render("Loading", True, (0,255,0))
-            pygame.mixer.init(44100)
-            pygame.mixer.music.load((path + songs[i]))
+            pygame.mixer.init(44100) #set frequency of mp3 file
+            pygame.mixer.music.load((path + songs[i])) #load mp3 file
             loaded = True
-        if data == b'01':
+        if data == b'01': #serial outputs "01" meaning play/pause button is pressed - should play song
             if loaded == False:
-                print('Load a song!')
+                print('Load a song!') #make sure song is loaded before clicking play/pause - will close out
             else:
                 if pygame.mixer.music.get_busy() == False:
                     pygame.mixer.music.play()
-                    pygame.mixer.music.set_volume(1)
                     player.label = player.myfont.render("Playing", True, (0,255,0))
                 else:
                     pygame.mixer.music.unpause()
                 print('Playing')
-        if data == b'02':
+        if data == b'02': #serial outputs "02" meaning play/pause is pressed - should pause song
             if loaded == False:
                 print('Load a song!')
-                player.label = player.myfont.render("Load a song!", True, (0,255,0))
+                player.label = player.myfont.render("Load a song!", True, (0,255,0)) #make sure song is loaded
             else:
                 pygame.mixer.music.pause()
                 player.label = player.myfont.render("Paused", True, (0,255,0))
                 print('Paused')
-                if pygame.mixer.music.get_busy() == False:
+                if pygame.mixer.music.get_busy() == False: #send data to arduino to stop motor and servo
                     arduino.write(b'3')
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #close window
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+                if event.key == pygame.K_ESCAPE: #hit escape to also close window
                     pygame.quit()
                     sys.exit()
-            if event.type == END_MUSIC_EVENT and event.code == 0:
+            if event.type == END_MUSIC_EVENT and event.code == 0:  #send data to arduino to stop motor and servo
                 arduino.write(b'3')
+        #update screen to say what state/button has been pressed
         player.screen.fill(player.blue)
         pygame.display.update()
         player.screen.blit(player.label, (320 - player.label.get_width() // 2, 240 - player.label.get_height() // 2))
         pygame.display.flip()
 
 musicplayer = Music_Player()
-files = musicplayer.filesfolder()
-events(musicplayer,songs)
+songs = musicplayer.filesfolder()
+events(musicplayer,songs) #run code
